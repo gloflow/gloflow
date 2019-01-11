21a9f87ae9bb74adac7473159d4536d73b75f896
Ivan Trajkovic <ivan.ebiz@gmail.com>
2019-01-10 23:21:21 -0500

    generalized build script to work for both gf_publisher_service and gf_images_service

--- ops/build.py
+++ ops/build.py
@@ -14,16 +14,29 @@ print ''
 
 
 #---------------------------------
-def build__gf_images():
-    print ' -- build %sgf_images%s service'%(fg('green'),attr(0))
-    gf_images_service__path_str = '%s/../go/apps/gf_images/gf_images_service.go'%(cwd_str)
-    output_path_str             = '%s/../bin/gf_images_service'%(cwd_str)
-    c                           = 'go build -o %s %s'%(output_path_str,gf_images_service__path_str,)
+#META
+gf_images_service__path_str        = '%s/../go/apps/gf_images/gf_images_service.go'%(cwd_str)
+gf_images_service__output_path_str = '%s/../bin/gf_images_service'%(cwd_str)
+
+gf_publisher_service__path_str        = '%s/../go/apps/gf_publisher/gf_publisher_service.go'%(cwd_str)
+gf_publisher_service__output_path_str = '%s/../bin/gf_publisher_service'%(cwd_str)
+#---------------------------------
+def build__go_bin(p_name_str,
+    p_main_go_file_path_str,
+    p_output_path_str):
+
+    print ' -- build %s%s%s service'%(fg('green'), p_name_str, attr(0))
+    
+    cwd_str = os.getcwd()
+    os.chdir(os.path.dirname(p_main_go_file_path_str)) #change into the target main package dir
+
+    c = 'go build -o %s'%(p_output_path_str)
     print c
     r = delegator.run(c)
     if not r.out == '': print r.out
     if not r.err == '': print '%sFAILED%s >>>>>>>\n%s'%(fg('red'),attr(0),r.err)
+
+    os.chdir(cwd_str) #return to initial dir
 #---------------------------------
-
-
-build__gf_images()
+#build__go_bin('gf_image_service',    gf_images_service__path_str,   gf_images_service__output_path_str)
+build__go_bin('gf_publisher_service',gf_publisher_service__path_str,gf_publisher_service__output_path_str)
