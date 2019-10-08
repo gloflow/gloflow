8f06a8517d4680235f2427e86a0428539b346f6a
Ivan Trajkovic <ivan.ebiz@gmail.com>
2019-10-08 19:46:17 -0400

    gf_builder.py is now running gf_build_changes module functions to get a spec of which apps changed in this monorepo. gf_builder Dockerfile (container) is no longer adding or running Py code directly (since the Drone workspace is now in the container home dir /home/gf)

--- build/gf_builder/gf_builder.py
+++ build/gf_builder/gf_builder.py
@@ -16,7 +16,6 @@ print("BUILD...")
 import delegator
 print(delegator.run("ls -al").out)
 print(delegator.run("pwd").out)
-print(delegator.run("ls -al /home/gf").out)
 
 
 
@@ -27,4 +26,20 @@ cwd_str = os.path.abspath(os.path.dirname(__file__))
 
 
 
-sys.path.append('%s'%(cwd_str))
+
+
+sys.path.append('%s/../../meta'%(cwd_str))
+import gf_meta
+
+
+sys.path.append('%s/../../ops/utils'%(cwd_str))
+import gf_build_changes
+
+
+
+print("DIFF")
+apps_changes_deps_map = gf_meta.get()['apps_changes_deps_map']
+
+
+changed_apps_map = gf_build_changes.list_changed_apps(apps_changes_deps_map)
+gf_build_changes.view_changed_apps(changed_apps_map)
