170476b07f12a57d7e27741292bbc5285c5164be
Ivan Trajkovic <ivan.ebiz@gmail.com>
2020-04-07 00:26:37 -0400

    more improvements to x.509 cert chain generation functions using CFSSL.

--- ops/utils/sec/gf_certs.py
+++ ops/utils/sec/gf_certs.py
@@ -19,98 +19,161 @@ import os, sys
 modd_str = os.path.abspath(os.path.dirname(__file__)) # module dir
 
 import time
+from colored import fg, bg, attr
 
 sys.path.append("%s/../../../go/gf_core/py"%(modd_str))
 import gf_core_cli
 
 #---------------------------------------------------
-def generate__cert_root(p_output_files_base_str,
-	p_config__file_path_str):
+# ROOT
+def generate__ca_root(p_output_files_base_str,
+	p_config__file_path_str,
+	p_sudo_bool = False):
 	assert os.path.isfile(p_config__file_path_str)
 	assert p_config__file_path_str.endswith(".json")
-
-
 	
+	print("%sGENERATE ROOT_CA%s"%(fg("yellow"), attr(0)))
 
+	c_lst = []
 
-	# # ALREADY_EXISTS
-	# if os.path.isfile(p_privkey_output__file_path_str) and \
-	#     os.path.isfile(p_cert_output_file_path_str):
-	#     print("CERT_ROOT ALREADY EXISTS")
-	#     print("privkey - %s"%(os.path.abspath(p_privkey_output__file_path_str)))
-	#     print("cert    - %s"%(os.path.abspath(p_cert_output_file_path_str)))
-	#
-	#     if gf_core_cli.confirm("recreate cert_root?"):
-	#         # archive existing cert by renaming it, and allow for new cert to get created
-	#         archive__old_cert(p_privkey_output__file_path_str,
-	#             p_cert_output_file_path_str)
-	#     else:
-	#         return
-
-
-
+	#-----------------
+	# GENERATE
+	if p_sudo_bool:
+		c_lst.append("sudo")
 	
-	c_lst = [
-		# "-initca" - initialise new CA
-		"cfssl gencert -initca %s"%(p_config__file_path_str),
-		"|",
+	c_lst.extend([
+		"cfssl gencert",
+		"-initca", # "-initca" - initialise new CA
+		p_config__file_path_str
+	])
 
+	#-----------------
+	c_lst.append("|")
+
+	#-----------------
+	# SAVE_TO_FS
+	if p_sudo_bool:
+		c_lst.append("sudo")
+
+	c_lst.extend([
 		# "-bare" - the response from CFSSL is not wrapped in the API standard response
 		"cfssljson -bare %s"%(p_output_files_base_str)
-	]
+	])
 
+	#-----------------
 	c_str = " ".join(c_lst)
 	_, _, return_code = gf_core_cli.run_cmd(c_str, p_env_map = None)
 	if not return_code == 0:
 		print("CLI failed...")
 		exit()
 
-#-------------------------------------------------------------
-def generate__cert_intermediate():
-
-	True
+	print("%sdone...%s"%(fg("green"), attr(0)))
 
 #-------------------------------------------------------------
-def generate__cert_leaf():
+# INTERMEDIATE
+def generate__ca_intermediate(p_output_files_base_str,
+	p_root_ca_base_str,
+	p_config__file_path_str,
+	p_config_csr__file_path_str,
+	p_profile_name_str = None,
+	p_sudo_bool        = False):
+	assert os.path.isfile(p_config__file_path_str)
+	assert os.path.isfile(p_config_csr__file_path_str)
 
-	# CERT_LEAF__SERVER
-	c_lst = [
-		"echo '{}'",
-		"|",
+	print("%sGENERATE INTERMEDIATE_CA%s"%(fg("yellow"), attr(0)))
+
+	# ROOT_CA
+	root_ca_cert__file_path_str = "%s.pem"%(p_root_ca_base_str)
+	root_ca_key__file_path_str  = "%s-key.pem"%(p_root_ca_base_str)
+
+	c_lst = []
+	
+	#-----------------
+	# GENERATE
+	if p_sudo_bool:
+		c_lst.append("sudo")
+
+	c_lst.extend([
 		"cfssl gencert",
-		"-ca=nomad-ca.pem",
-		"-ca-key=nomad-ca-key.pem",
-		"-config=cfssl.json",
+
+		# ROOT_CA
+		"-ca %s"%(root_ca_cert__file_path_str),
+		"-ca-key %s"%(root_ca_key__file_path_str),
+
+		"-config %s"%(p_config__file_path_str)
+	])
+
+	if not p_profile_name_str == None:
+		c_lst.append("-profile %s"%(p_profile_name_str))
+
+	c_lst.append(p_config_csr__file_path_str)
+
+	#-----------------
+	c_lst.append("|")
+
+	#-----------------
+	# SAVE_TO_FS
+	if p_sudo_bool:
+		c_lst.append("sudo")
+
+	c_lst.extend([
+		"cfssljson -bare %s"%(p_output_files_base_str)
+	])
+	
+	#-----------------
+	c_str = " ".join(c_lst)
+	_, _, return_code = gf_core_cli.run_cmd(c_str, p_env_map = None)
+	if not return_code == 0:
+		print("CLI failed...")
+		exit()
+
+	print("%sdone...%s"%(fg("green"), attr(0)))
+
+#-------------------------------------------------------------
+# LEAF
+def generate__cert_leaf(p_output_files_base_str,
+	p_ca_base_str,
+	p_config__file_path_str):
+
+	# INTERMEDIATE_CA
+	ca_cert__file_path_str = "%s.pem"%(p_ca_base_str)
+	ca_key__file_path_str  = "%s-key.pem"%(p_ca_base_str)
+
+	c_lst = []
+	
+	#-----------------
+	# GENERATE
+	if p_sudo_bool:
+		c_lst.append("sudo")
+
+	c_lst.extend([
+		"cfssl gencert",
+
+		# INTERMEDIATE_CA
+		"-ca=%s"%(ca_cert__file_path_str),
+		"-ca-key=%s"%(ca_key__file_path_str),
+		"-config=%s"%(p_config__file_path_str),
 		'''-hostname="server.global.nomad,localhost,127.0.0.1"''', "-",
-		"|",
-		"cfssljson -bare server"
-	]
+	])
 
-	# CERT_LEAF__CLIENT
-	c_lst = [
-		"echo '{}'",
-		"|",
-		"cfssl gencert",
-		"-ca=nomad-ca.pem",
-		"-ca-key=nomad-ca-key.pem",
-		"-config=cfssl.json",
-		'''-hostname="client.global.nomad,localhost,127.0.0.1"''', "-",
-		"|",
-		"cfssljson -bare client"
-	]
+	#-----------------
+	c_lst.append("|")
 
-	# CERT_LEAF__CLI
-	c_lst = [
-		"echo '{}'",
-		"|",
-		"cfssl gencert",
-		"-ca=nomad-ca.pem",
-		"-ca-key=nomad-ca-key.pem",
-		"-profile=client", "-",
-		"|",
-		"cfssljson -bare cli"
-	]
+	#-----------------
+	# SAVE_TO_FS
+	if p_sudo_bool:
+		c_lst.append("sudo")
 
+	c_lst.append("cfssljson -bare %s"%(p_output_files_base_str))
+
+	#-----------------
+	c_str = " ".join(c_lst)
+	_, _, return_code = gf_core_cli.run_cmd(c_str, p_env_map = None)
+	if not return_code == 0:
+		print("CLI failed...")
+		exit()
+
+	print("%sdone...%s"%(fg("green"), attr(0)))
 
 #-------------------------------------------------------------
 def archive_if_exists(p_files_base_str, p_sudo_bool = False):
@@ -124,7 +187,13 @@ def archive_if_exists(p_files_base_str, p_sudo_bool = False):
 	# list all files in target dir
 	# "-1"  - force output to be one entry per line
 	# "^%s" - pattern matches the file_base only at the start of the line
-	stdout_str, _, return_code = gf_core_cli.run_cmd("%s ls -1 %s | grep '^%s'"%(sudo_str, dir_str, file_base_str), p_env_map = None, p_print_output_bool = False)
+	stdout_str, _, return_code = gf_core_cli.run_cmd("%s ls -1 %s | grep '^%s'"%(sudo_str, dir_str, file_base_str),
+		p_env_map = None,
+		p_print_output_bool = True)
+	
+	if stdout_str == "":
+		return True
+
 	stdout_clean_str = stdout_str.strip()
 	lines_lst        = stdout_clean_str.split("\n")
 
