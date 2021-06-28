d666215993103ad4738c1e3376eb7dac4e667296
Ivan Trajkovic <ivan.ebiz@gmail.com>
2021-06-27 21:22:29 -0400

    fixed gf_images web dev files, which help with local development. added tiny readme for that.

--- web/src/gf_apps/gf_images/dev/build_gf_images_dashboard.py
+++ web/src/gf_apps/gf_images/dev/build_gf_images_dashboard.py
@@ -6,16 +6,14 @@ import envoy
 
 output_file_str = './bin/gf_images_dashboard.js'
 files_lst = [
-	'./src/dashboard/gf_images_dashboard.ts',
-	'./src/dashboard/gf_images_debug.ts',
-	'./src/dashboard/gf_images_stats.ts',
+	'./../ts/gf_images_dashboard/gf_images_dashboard.ts',
+	'./../ts/gf_images_dashboard/gf_images_stats.ts',
 ]
 
 print 'files_lst - %s'%(files_lst)
 
 print 'RUNNING COMPILE...'
-c = 'tsc --out %s %s'%(output_file_str,
-					' '.join(files_lst))
+c = 'tsc --out %s %s'%(output_file_str, ' '.join(files_lst))
 print c
 
 r = envoy.run(c)
