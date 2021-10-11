59a4e13cc9f4da57cf57e68c22c7932651eeabb3
Ivan Trajkovic <ivan.ebiz@gmail.com>
2021-10-11 00:01:23 -0400

    core gf_rpc_lib addition of SSE functions.

--- go/gf_rpc_lib/gf_rpc_client.go
+++ go/gf_rpc_lib/gf_rpc_client.go
@@ -29,16 +29,130 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"context"
+	"strings"
+	"bufio"
 	"github.com/fatih/color"
 	"github.com/gloflow/gloflow/go/gf_core"
 	// "github.com/davecgh/go-spew/spew"
 )
 
 //-------------------------------------------------
+func Client__request_sse(p_url_str string,
+	p_resp_data_ch chan(map[string]interface{}),
+	p_headers_map  map[string]string,
+	p_ctx          context.Context,
+	p_runtime_sys  *gf_core.Runtime_sys) *gf_core.GF_error {
+
+	//-----------------------
+	// FETCH_URL
+
+
+	fmt.Println("7777777777777777777777777")
+	
+	user_agent_str := "gf_rpc_client"
+	gf_http_fetch, gf_err := gf_core.HTTP__fetch_url(p_url_str,
+		p_headers_map,
+		user_agent_str,
+		p_ctx,
+		p_runtime_sys)
+	if gf_err != nil {
+		return gf_err
+	}
+
+	fmt.Println("888888888888888888888888888888")
+
+
+
+	//-----------------------
+
+	http_resp   := gf_http_fetch.Resp
+	resp_reader := bufio.NewReader(http_resp.Body)
+
+	for {
+
+		line_lst, err := resp_reader.ReadBytes('\n')
+		if err != nil {
+			
+			gf_err := gf_core.Error__create("failed to read a line in body reasponse stream for client http sse connection",
+				"io_reader_error",
+				map[string]interface{}{"url_str": p_url_str,},
+				nil, "gf_rpc_lib", p_runtime_sys)
+			return gf_err
+		}
+
+		line_str := string(line_lst)
+
+		// filter out keep-alive new lines
+		if line_str != "" && strings.HasPrefix(line_str, "data: ") {
+
+
+			msg_str := strings.Replace(line_str, "data: ", "", 1)
+			msg_map := map[string]interface{}{}
+			err     := json.Unmarshal([]byte(msg_str), &msg_map)
+
+			if err != nil {
+				gf_err := gf_core.Error__create("failed to parse JSON response line of the SSE stream (of even updates from a gf_images server)",
+					"json_unmarshal_error",
+					map[string]interface{}{
+						"url_str":  p_url_str,
+						"line_str": line_str,
+					},
+					err, "gf_rpc_lib", p_runtime_sys)
+				return gf_err
+			}
+
+			//-------------------
+			// STATUS
+			if _, ok := msg_map["status_str"]; !ok {
+				gf_err := gf_core.Error__create("sse message json doesnt container key status_str",
+					"verify__missing_key_error",
+					map[string]interface{}{
+						"url_str":  p_url_str,
+						"line_str": line_str,
+					},
+					nil, "gf_rpc_lib", p_runtime_sys)
+				return gf_err
+			}
+			status_str := msg_map["status_str"].(string)
+
+			if !(status_str == "ok" || status_str == "error") {
+				gf_err := gf_core.Error__create("sse message json status_str key is not of value ok|error",
+					"verify__invalid_key_value_error",
+					map[string]interface{}{
+						"status_str": status_str,
+						"url_str":    p_url_str,
+						"line_str":   line_str,
+					},
+					nil, "gf_rpc_lib", p_runtime_sys)
+				return gf_err
+			}
+
+			//-------------------
+			// DATA
+			if _, ok := msg_map["data_map"]; !ok {
+				gf_err := gf_core.Error__create("sse message json doesnt container key data_map",
+					"verify__missing_key_error",
+					map[string]interface{}{"msg_map": msg_map,},
+					nil, "gf_rpc_lib", p_runtime_sys)
+				return gf_err
+			}
+			
+			data_map := msg_map["data_map"].(map[string]interface{})
+			
+			//-------------------
+			
+			p_resp_data_ch <- data_map
+		}
+	}
+	
+	return nil
+}
+
+//-------------------------------------------------
 func Client__request(p_url_str string,
 	p_headers_map map[string]string,
 	p_ctx         context.Context,
-	p_runtime_sys *gf_core.Runtime_sys) (map[string]interface{}, *gf_core.Gf_error) {
+	p_runtime_sys *gf_core.Runtime_sys) (map[string]interface{}, *gf_core.GF_error) {
 
 	yellow   := color.New(color.FgYellow).SprintFunc()
 	yellowBg := color.New(color.FgBlack, color.BgYellow).SprintFunc()
