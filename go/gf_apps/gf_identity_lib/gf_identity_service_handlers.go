643f0c81937410a1ff9a6331875f38d71fe6fe7f
Ivan Trajkovic <ivan.ebiz@gmail.com>
2021-12-31 21:43:24 -0500

    gf_identity fix

--- go/gf_apps/gf_identity_lib/gf_identity_service_handlers.go
+++ go/gf_apps/gf_identity_lib/gf_identity_service_handlers.go
@@ -20,12 +20,12 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 package gf_identity_lib
 
 import (
-	"fmt"
+	// "fmt"
 	"net/http"
 	"context"
 	"github.com/gloflow/gloflow/go/gf_core"
 	"github.com/gloflow/gloflow/go/gf_rpc_lib"
-	"github.com/davecgh/go-spew/spew"
+	// "github.com/davecgh/go-spew/spew"
 )
 
 //------------------------------------------------
