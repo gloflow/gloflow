c488a0e6e6c978df5c5e39493a7e6fc8c96ba623
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-01-28 22:43:09 -0500

    gf_landing_page web - small fix

--- web/src/gf_apps/gf_landing_page/dev/build.py
+++ web/src/gf_apps/gf_landing_page/dev/build.py
@@ -7,17 +7,17 @@ import delegator
 output_file_str = './bin/gf_landing_page.js'
 files_lst = [
 	# './../ts/gf_calc.ts',
-	'./../ts/gf_email_registration.ts',
+	# './../ts/gf_email_registration.ts',
 	'./../ts/gf_landing_page.ts',
-	'./../ts/gf_images.ts',
-	'./../ts/procedural_art/gf_procedural_art.ts',
-	'./../ts/procedural_art/gf_evolved_squares.ts',
+	# './../ts/gf_images.ts',
+	# './../ts/procedural_art/gf_procedural_art.ts',
+	# './../ts/procedural_art/gf_evolved_squares.ts',
 ]
 
 print('files_lst - %s'%(files_lst))
 
 print('RUNNING COMPILE...')
-r = delegator.run(f"tsc --module system --out {output_file_str} {' '.join(files_lst)}")
+r = delegator.run(f"tsc --module system --target es2017 --out {output_file_str} {' '.join(files_lst)}")
 
 print(r.out)
 print(r.err)
