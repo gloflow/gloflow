460fd40b5660bffbcea3a8a5b6546c29964c448e
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-01-29 22:03:13 -0500

    in gf_landing_page gf_image_colors fixes on_color_compute callback

--- web/src/gf_apps/gf_landing_page/ts/gf_images.ts
+++ web/src/gf_apps/gf_landing_page/ts/gf_images.ts
@@ -67,11 +67,11 @@ export function init(p_log_fun) {
 
 
 
-				console.log("image colors pallete", p_color_dominant_hex_str, p_colors_hexes_lst)
+				console.log("image colors pallete", p_color_dominant_hex_str, p_colors_hexes_lst, p_image_info_element)
 
-				$(p_image_info_element).css("background-color", p_color_dominant_hex_str);
-				$(p_image_info_element).find(".image_title").css("background-color", p_color_dominant_hex_str);
-				$(p_image_info_element).find(".origin_page_url").css("background-color", p_color_dominant_hex_str);
+				$(p_image_info_element).css("background-color", `#${p_color_dominant_hex_str}`);
+				$(p_image_info_element).find(".image_title").css("background-color", `#${p_color_dominant_hex_str}`);
+				$(p_image_info_element).find(".origin_page_url").css("background-color", `#${p_color_dominant_hex_str}`);
 
 			});
 
