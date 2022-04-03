a668cca443bf0811df0d3ab4dd27b62fdc27609d
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-04-03 23:00:41 +0200

    gf_themes

--- go/gf_themes/gf_theme.go
+++ go/gf_themes/gf_theme.go
@@ -20,9 +20,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 package gf_themes
 
 
-
-
 //---------------------------------------------------
 type GFtheme struct {
+	
+
+
 	
 }
