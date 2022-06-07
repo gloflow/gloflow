3676b2cd2df7479b9cf58ddc1c56959d9fde006e
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-06-07 22:55:15 +0200

    gf_web3 - test fixed

--- go/gf_web3/gf_web3_lib/t__address_test.go
+++ go/gf_web3/gf_web3_lib/t__address_test.go
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 package gf_web3_lib
 
 import (
-	"fmt"
+	// "fmt"
 	"testing"
 	"context"
 	"github.com/parnurzeal/gorequest"
