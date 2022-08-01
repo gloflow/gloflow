3b08d5d79956b47cdb14c8c748bf9d171afc7aec
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-08-01 03:30:01 +0200

    gf_core web - image_viewer fix for fitting the image in the browser window only if it is larger than window dimensions.

--- web/src/gf_core/ts/gf_image_viewer.ts
+++ web/src/gf_core/ts/gf_image_viewer.ts
@@ -27,7 +27,7 @@ export function init(p_image_element,
 	p_log_fun) {
 	p_log_fun("FUN_ENTER", "gf_image_viewer.init()");
 
-	//const img_thumb_medium_url = $(p_image_element).find('img').data('img_thumb_medium_url');
+	// const img_thumb_medium_url = $(p_image_element).find('img').data('img_thumb_medium_url');
 
 	$(p_image_element).find("img").click(()=>{
 
@@ -38,20 +38,21 @@ export function init(p_image_element,
 				<div id="background"></div>
 				<div id="image_detail">
 					<img src="${p_img_thumb_large_url_str}"></img>
+					<div class="flow_name">${p_flow_name_str}</div>
 				</div>
 			</div>`);
 
 		$('body').append(image_view);
 
-		const image_detail = $(image_view).find("#image_detail");
-		$(image_detail).css("position", "absolute");
+		const image_detail_element = $(image_view).find("#image_detail");
+		$(image_detail_element).css("position", "absolute");
 
 		//----------------------
 		// BAKCGROUND
 		const bg = $(image_view).find("#background");
 
 		// position the background vertically where the user has scrolled to
-		$(bg).css("top", $(window).scrollTop()+"px");
+		$(bg).css("top", `${$(window).scrollTop()}px`);
 
 		//----------------------
 		// IMPORTANT!! - turn off vertical scrolling while viewing the image
@@ -60,12 +61,15 @@ export function init(p_image_element,
 		//----------------------
 		// IMG_ONLOAD
 		$(image_view).find("img").on("load", ()=>{
-			image_position(image_detail);
+			image_position_and_scale(image_detail_element);
 		});
 
+		//-------------------------------------------------
 		function resize_handler() {
-			image_position(image_detail);
+			image_position_and_scale(image_detail_element);
 		}
+
+		//-------------------------------------------------
 		// reposition image detail if the window resizes
 		$(window).on("resize", resize_handler);
 
@@ -85,14 +89,40 @@ export function init(p_image_element,
 }
 
 //-------------------------------------------------
-function image_position(p_image_detail) {
+function image_position_and_scale(p_image_detail_element) {
 
 	// Math.max() - returns the largest of zero or more numbers.
 	// Math.max(10, 20);   //20
 	// Math.max(-10, -20); //-10
-	const image_x = Math.max(0, (($(window).width() - $(p_image_detail).outerWidth()) / 2) + $(window).scrollLeft());
-	const image_y = Math.max(0, (($(window).height() - $(p_image_detail).outerHeight()) / 2) + $(window).scrollTop());
+	const image_detail_x = Math.max(0, (($(window).width() - $(p_image_detail_element).outerWidth()) / 2) + $(window).scrollLeft());
+	const image_detail_y = Math.max(0, (($(window).height() - $(p_image_detail_element).outerHeight()) / 2) + $(window).scrollTop());
 
-	$(p_image_detail).css("left", image_x+"px");
-	$(p_image_detail).css("top",  image_y+"px");
+	$(p_image_detail_element).css("left", `${image_detail_x}px`);
+	$(p_image_detail_element).css("top",  `${image_detail_y}px`);
+
+
+
+
+
+
+
+	const window_width_int        = $(window).width() - 100;  // some padding removed from real window width
+	const window_height_int       = $(window).height() - 100; // some padding removed from real window height
+	const image_detail_width_int  = $(p_image_detail_element).outerHeight();
+	const image_detail_height_int = $(p_image_detail_element).outerHeight();
+
+
+
+	// WIDTH
+	// image view is larger than the window height, so scale it back
+	if (image_detail_width_int > window_width_int) {
+		$(p_image_detail_element).css("width", "90%");
+	}
+
+	// HEIGHT
+	// image view is larger than the window height, so scale it back
+	if (image_detail_height_int > window_height_int) {
+		$(p_image_detail_element).css("height", "90%");
+	}
+	
 }
