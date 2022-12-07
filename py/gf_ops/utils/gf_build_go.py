013f2e36806658b4a02a9c7cda6cbde9f6c98d9a
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-12-08 00:13:02 +0100

    py golang build function handles go modules cache better now on local dev machines, by mounting host dir for go cache.

--- py/gf_ops/utils/gf_build_go.py
+++ py/gf_ops/utils/gf_build_go.py
@@ -40,10 +40,25 @@ def run_in_cont(p_name_str,
         "--rm", # remove after exit
     ]
 
+    # FIX!! - run "go env GOMODCACHE" in build container as well to dynamically determine
+    #         where its module cache dir is, to avoid this hardcoded value (in case it changes)
+    #         and this code gets broken then.
+    host_go_module_cache_dir_str            = delegator.run("go env GOMODCACHE").out.strip()
+    build_container_go_module_cache_dir_str = "/root/go/pkg/mod"
+
     # VOLUMES
     volumes_lst = [
         # mount GF repo into the container
-        "-v", f"{repo_local_path_str}:/home/gf"
+        "-v", f"{repo_local_path_str}:/home/gf",
+
+        #-----------------------------
+        # IMPORTANT!!
+        # Go module cache - mount the host users go module cache dir into the
+        #                   build containers go module cache dir. this will drastically speed up builds
+        #                   on dev local machines since it will not download modules each time.
+        "-v", f"{host_go_module_cache_dir_str}:{build_container_go_module_cache_dir_str}"
+
+        #-----------------------------
     ]
 
     cmd_lst.extend(volumes_lst)
@@ -84,8 +99,8 @@ def run(p_name_str,
     assert os.path.isdir(os.path.dirname(p_go_output_path_str))
 
     print("--------------------")
-    print(f"Go {fg('yellow')}cache dir{attr(0)}:")
-    gf_core_cli.run(f"go env GOCACHE")
+    print(f"GO ENV\n")
+    gf_core_cli.run(f"go env")
     print("--------------------")
 
     cwd_str = os.getcwd()
@@ -93,6 +108,9 @@ def run(p_name_str,
 
     #--------------------------------------------------
     def get_libs_for_linking():
+
+        print("--------------------")
+        print("LIBS_LINKIN\n")
 
         # RUST_DYNAMIC_LIBS
         dynamic_libs_dir_path_str    = os.path.abspath(f"{modd_str}/../../../rust/build")
@@ -107,14 +125,18 @@ def run(p_name_str,
             tf_dynamic_libs_dir_path_str
         ]
         LD_paths_str = f"LD_LIBRARY_PATH={':'.join(LD_paths_lst)}"
+
+        print("--------------------")
+
         return LD_paths_str
 
     #--------------------------------------------------
     LD_paths_str = get_libs_for_linking()
 
     # GO_GET
+    # DEPRECATED!! - on modern Golang version "go build" will download modules as well,
+    #                so there is no need for a "go get" to be run.
     if p_go_get_bool:
-
         _, _, exit_code_int = gf_core_cli.run(f"{LD_paths_str} go get -u") 
         print("")
         print("")
@@ -129,7 +151,8 @@ def run(p_name_str,
     c_str = None
     if p_static_bool:
         
-        print(f"{fg('yellow')}STATIC LINKING{attr(0)} --")
+        print("--------------------")
+        print(f"{fg('yellow')}STATIC LINKING{attr(0)}\n")
         
         # https://golang.org/cmd/link/
         # IMPORTANT!! - "CGO_ENABLED=0" and "-installsuffix cgo" no longer necessary since golang 1.10.
@@ -180,7 +203,8 @@ def run(p_name_str,
     #-----------------------------
     # DYNAMIC_LINKING - fast build for dev.
     else:
-        print(f"{fg('yellow')}DYNAMIC LINKING{attr(0)} --")
+        print("--------------------")
+        print(f"{fg('yellow')}DYNAMIC LINKING{attr(0)}\n")
 
         c_str = f"{LD_paths_str} go build -buildvcs=false -o {p_go_output_path_str}"
 
@@ -197,4 +221,5 @@ def run(p_name_str,
 
     os.chdir(cwd_str) # return to initial dir
 
+    print("--------------------")
     print("build done...")
