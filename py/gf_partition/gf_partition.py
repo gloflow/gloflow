50da53afbfe4178c27759e2e197bf5775efbf197
Ivan Trajkovic <ivan.ebiz@gmail.com>
2023-05-11 22:55:06 +0200

    gf_partition - started adding first drafts of code for the py data partitioning functionality

---
+++ py/gf_partition/gf_partition.py
@@ -0,0 +1,172 @@
+# GloFlow application and media management/publishing platform
+# Copyright (C) 2023 Ivan Trajkovic
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os, sys
+modd_str = os.path.abspath(os.path.dirname(__file__)) # module dir
+
+sys.path.append(f"{modd_str}/../gf_core")
+import gf_core_sql_db
+
+#---------------------------------------------------------------------------------
+def create(p_partition_map,
+    p_db_client):
+    assert isinstance(p_partition_map, dict)
+
+    print("create partition...")
+
+    table_name_str = "gf_data_partitions"
+
+    query_str = f"""
+        INSERT INTO {table_name_str} (
+            set_name,
+            group_name,
+            dagster_asset_id,
+            partition_i,
+            partition_size,
+            materialized) 
+        VALUES (%s, %s, %s, %s, %s, %s)"""
+
+    values_tpl = (p_partition_map["set_name_str"],
+        p_partition_map["group_name_str"],
+        p_partition_map["dagster_asset_id_str"],
+        p_partition_map["partition_i_int"],
+        p_partition_map["partition_size_int"], True)
+
+    cur = p_db_client.cursor()
+    cur.execute(query_str, values_tpl)
+
+    p_db_client.commit()
+
+    cur.close()
+    
+#---------------------------------------------------------------------------------
+def check_exists(p_set_name_str,
+    p_group_name_str,
+    p_partition_i_int,
+    p_partition_size_int,
+    p_db_client):
+    
+    table_name_str = "gf_data_partitions"
+    partition_str = "%s:%s:%s:%s"%(p_set_name_str, p_group_name_str, p_partition_i_int, p_partition_size_int)
+
+    try:
+        result = get_one(p_set_name_str,
+            p_group_name_str,
+            p_partition_i_int,
+            p_partition_size_int,
+            p_db_client)
+
+    except Exception as e:
+        if isinstance(e, errors.UndefinedTable):
+            # whole table doesnt exist, not just the partition, so report that
+            return False
+        else:
+            # rethrow the exception further up the chain
+            raise
+    
+    if result is not None:
+        
+        materialized_bool = result[0]
+
+        print(f"partition exists - {partition_str}")
+        return True, materialized_bool
+    else:
+        print(f"partition doesnt exists - {partition_str}")
+        return False, False
+
+    cur.close()
+    p_db_client.close()
+
+#---------------------------------------------------------------------------------
+def get_one(p_set_name_str,
+    p_group_name_str,
+    p_partition_i_int,
+    p_partition_size_int,
+    p_db_client):
+
+    table_name_str = "gf_data_partitions"
+    cur = p_db_client.cursor()
+    query_str = f"""
+        SELECT *
+        FROM {table_name_str}
+        WHERE set_name = %s AND group_name = %s AND partition_i = %s AND partition_size = %s
+    """
+
+    # QUERY
+    cur.execute(query_str,
+        (p_set_name_str, p_group_name_str, p_partition_i_int, p_partition_size_int))
+
+    result = cur.fetchone()
+    return result
+
+#---------------------------------------------------------------------------------
+def init_db(p_db_client):
+    
+    cur = p_db_client.cursor()
+
+    table_name_str = "gf_data_partitions"
+    table_partition_materialize_name_str = "gf_data_partitions_materilize"
+
+
+    #---------------------------------------------------------------------------------
+    def create_table():
+
+        # cur.execute(f"DROP TABLE {table_name_str} CASCADE;")
+        # cur.execute(f"DROP TABLE {table_partition_materialize_name_str} CASCADE;")
+
+        # GF_DATA_PARTITIONS
+        if not gf_core_sql_db.table_exists(table_name_str, cur):
+
+            # dagster_asset_id - rereference to the appropriate Dagster Asset that
+            #                    materialized/computed this partition.
+            # materialized_bool - has this partition been ever materialized?
+            create_table_sql_str = f"""
+                CREATE TABLE {table_name_str} (
+                    id               BIGSERIAL PRIMARY KEY,
+                    created_at       TIMESTAMP DEFAULT NOW(),
+                    set_name         VARCHAR(255) NOT NULL,
+                    group_name       VARCHAR(255) NOT NULL,
+                    dagster_asset_id VARCHAR(255) NOT NULL,
+
+                    partition_i    INT NOT NULL,
+                    partition_size INT NOT NULL,
+
+                    materialized BOOLEAN NOT NULL,
+
+                    UNIQUE(set_name, group_name, partition_i, partition_size)
+                );"""
+            cur.execute(create_table_sql_str)
+            p_db_client.commit()
+
+        # GF_DATA_PARTITIONS_MATERIALIZE
+        if not gf_core_sql_db.table_exists(table_partition_materialize_name_str, cur):
+
+            # status - "started" | "failed" | "completed"
+            create_table_sql_str = f"""
+                CREATE TABLE {table_partition_materialize_name_str} (
+                    id           BIGSERIAL PRIMARY KEY,
+                    created_at   TIMESTAMP DEFAULT NOW(),
+                    set_name     VARCHAR(255) NOT NULL,
+                    group_name   VARCHAR(255) NOT NULL,
+                    status       VARCHAR(255) NOT NULL,
+                    partition_id BIGINT REFERENCES {table_name_str}(id)
+                );"""
+            cur.execute(create_table_sql_str)
+            p_db_client.commit()
+    
+    #---------------------------------------------------------------------------------
+    create_table()
