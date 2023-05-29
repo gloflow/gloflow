865c46495a8acfaa25bbd96658868c51e4eb934c
Ivan Trajkovic <ivan.ebiz@gmail.com>
2023-05-29 21:27:41 +0200

    py youtube - getting channel info functions added

--- py/gf_extern_services/gf_youtube/gf_youtube.py
+++ py/gf_extern_services/gf_youtube/gf_youtube.py
@@ -97,7 +97,8 @@ def search(p_username_str,
     return channels_lst, videos_lst
 
 #---------------------------------------------------------------------------------
-def channel_info(p_username_str):
+def channel_info_by_username(p_username_str,
+    p_service_client):
 
     # IMPORTANT!! - most usernames dont return any results for channels.
     #               in Youtube there is a privacy option for channel owners to disable
@@ -107,3 +108,77 @@ def channel_info(p_username_str):
             forUsername=p_username_str
         ).execute()
     print(r)
+
+#---------------------------------------------------------------------------------
+def channel_info_by_id(p_channel_id_str,
+    p_service_client):
+
+    # IMPORTANT!! - most usernames dont return any results for channels.
+    #               in Youtube there is a privacy option for channel owners to disable
+    #               their channel info being returned by Youtube API.
+    r = p_service_client.channels().list(
+            part="snippet,contentDetails,statistics",
+            id=p_channel_id_str
+        ).execute()
+
+    # print(r)
+
+    #---------------------------------------------------------------------------------
+    def debug_view():
+        if r['items']:
+            channel_info_map = r['items'][0]
+            print(f"Title: {channel_info_map['snippet']['title']}")
+            print(f"Description: {channel_info_map['snippet']['description']}")
+            print(f"Published at: {channel_info_map['snippet']['publishedAt']}")
+            print(f"Number of subscribers: {channel_info_map['statistics']['subscriberCount']}")
+            print(f"Total views: {channel_info_map['statistics']['viewCount']}")
+            print(f"Total videos: {channel_info_map['statistics']['videoCount']}")
+        else:
+            print(f"No channel found for channel ID '{p_channel_id_str}'.")
+
+    #---------------------------------------------------------------------------------
+
+    debug_view()
+
+    channel_info_map = r['items'][0]
+    title_str = channel_info_map['snippet']['title']
+    descr_str = channel_info_map['snippet']['description']
+    published_at_str     = channel_info_map['snippet']['publishedAt']
+    subscriber_count_int = channel_info_map['statistics']['subscriberCount']
+    view_count_int       = channel_info_map['statistics']['viewCount']
+    video_count_int      = channel_info_map['statistics']['videoCount']
+
+    channel_meta_map = {
+        "title_str": title_str,
+        "descr_str": descr_str,
+        "published_at_str": published_at_str,
+        "subscriber_count_int": subscriber_count_int,
+        "view_count_int":       view_count_int,
+        "video_count_int":      video_count_int,
+    }
+    return channel_meta_map
+
+#---------------------------------------------------------------------------------
+def channel_videos(p_channel_id_str,
+    p_service_client,
+    p_results_num_int=20):
+
+    r = p_service_client.search().list(
+        channelId=p_channel_id_str,
+        part="id",
+        maxResults=p_results_num_int
+    ).execute()
+
+#---------------------------------------------------------------------------------
+def video_info(p_video_id_str,
+    p_service_client):
+
+    r = p_service_client.videos().list(
+        part='snippet,statistics',
+        id=p_video_id_str
+    ).execute()
+
+    for video_result_map in r.get("items", []):
+        print(f"Title: {video_result_map['snippet']['title']}")
+        print(f"Thumbnail: {video_result_map['snippet']['thumbnails']['default']['url']}")
+        print(f"View count: {video_result_map['statistics']['viewCount']}")
