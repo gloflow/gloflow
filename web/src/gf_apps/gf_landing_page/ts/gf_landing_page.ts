c748e0d544a9f6ffda2bbad3d851fbe214f38b67
Ivan Trajkovic <ivan.ebiz@gmail.com>
2023-06-17 21:13:16 +0200

    web - gf_landing_page document.ready() addition where its called only if that module is a top level module - to facilitate importing of that module by external TS modules.

--- web/src/gf_apps/gf_landing_page/ts/gf_landing_page.ts
+++ web/src/gf_apps/gf_landing_page/ts/gf_landing_page.ts
@@ -32,25 +32,32 @@ import * as gf_image_colors   from "./../../../gf_core/ts/gf_image_colors";
 declare var gf_upload__init;
 
 //--------------------------------------------------------
-$(document).ready(()=>{
-	//-------------------------------------------------
-	function log_fun(p_g, p_m) {
-		var msg_str = p_g+':'+p_m;
-		switch (p_g) {
-			case "INFO":
-				console.log("%cINFO"+":"+"%c"+p_m, "color:green; background-color:#ACCFAC;", "background-color:#ACCFAC;");
-				break;
-			case "FUN_ENTER":
-				console.log("%cFUN_ENTER"+":"+"%c"+p_m, "color:yellow; background-color:lightgray", "background-color:lightgray");
-				break;
+// check if this file is running in global scope, and only if it is should the
+// document.ready() function be called.
+// this is needed in case that this module is imported in other external parent TS modules,
+// where init() is called, but where we dont want this document.ready() function to be called
+// (where there might be other parent document.ready() calls)
+if (window === this) {
+	$(document).ready(()=>{
+		//-------------------------------------------------
+		function log_fun(p_g, p_m) {
+			var msg_str = p_g+':'+p_m;
+			switch (p_g) {
+				case "INFO":
+					console.log("%cINFO"+":"+"%c"+p_m, "color:green; background-color:#ACCFAC;", "background-color:#ACCFAC;");
+					break;
+				case "FUN_ENTER":
+					console.log("%cFUN_ENTER"+":"+"%c"+p_m, "color:yellow; background-color:lightgray", "background-color:lightgray");
+					break;
+			}
 		}
-	}
 
-	//-------------------------------------------------
-	$("time.timeago").timeago();
+		//-------------------------------------------------
+		$("time.timeago").timeago();
 
-	init(log_fun);
-});
+		init(log_fun);
+	});
+}
 
 //--------------------------------------------------------
 // INIT
