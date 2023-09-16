bb60733788968b99cdee12992ae4327acb273097
Ivan Trajkovic <ivan.ebiz@gmail.com>
2023-09-16 14:24:55 +0200

    gf_tagger web - tag_objects added needed css references and sys_panel init

---
+++ web/src/gf_apps/gf_tagger/ts/gf_tag_objects/gf_tag_objects_lib.ts
@@ -0,0 +1,36 @@
+/*
+GloFlow application and media management/publishing platform
+Copyright (C) 2023 Ivan Trajkovic
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+///<reference path="../../../../d/jquery.d.ts" />
+///<reference path="../../../../d/masonry.layout.d.ts" />
+///<reference path="../../../../d/jquery.timeago.d.ts" />
+
+import * as gf_image_viewer from "./../../../../gf_core/ts/gf_image_viewer";
+import * as gf_sys_panel from "./../../../../gf_sys_panel/ts/gf_sys_panel";
+
+// GF_GLOBAL_JS_FUNCTION - included in the page from gf_core (.js file)
+declare var gf_upload__init;
+
+//-------------------------------------------------
+export function init(p_log_fun) {
+
+
+
+    gf_sys_panel.init_with_auth(p_log_fun);
+}
