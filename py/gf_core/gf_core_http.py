2e5644551e1eb2375d5564f0f046bb0ba57822ca
Ivan Trajkovic <ivan.ebiz@gmail.com>
2023-12-20 14:06:38 +0100

    gf_core py - added function for downloading a file

--- py/gf_core/gf_core_http.py
+++ py/gf_core/gf_core_http.py
@@ -15,6 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import os
 import requests
 
 #---------------------------------------------------------------------------------
@@ -24,3 +25,25 @@ def download_file(p_url_str, p_local_file_path_str):
         f.write(response.content)
 
 #---------------------------------------------------------------------------------
+def download_file_chunked(p_url_str,
+    p_local_file_path_str):
+    """Download an image from a URL and save it to the local filesystem."""
+    
+    # HTTP_GET
+    response = requests.get(p_url_str, stream=True)
+
+    if response.status_code == 200:
+        
+        # ensure the local directory exists
+        if not os.path.dirname(p_local_file_path_str) == "":
+            os.makedirs(os.path.dirname(p_local_file_path_str), exist_ok=True)
+
+        # open the local file in binary write mode
+        with open(p_local_file_path_str, 'wb') as file:
+
+            for chunk in response.iter_content(1024):
+                file.write(chunk)
+        
+        print(f"image downloaded: {p_local_file_path_str}")
+    else:
+        print(f"failed to download image. Status code: {response.status_code}")
