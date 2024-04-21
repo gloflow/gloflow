cff4e0c93b9a024f275780aee567bf4acf4fd58c
Ivan Trajkovic <ivan.ebiz@gmail.com>
2024-04-21 12:38:42 +0200

    gf_maps web - added dedicated css file and modified pages meta

--- meta/gf_web_meta.py
+++ meta/gf_web_meta.py
@@ -68,7 +68,6 @@ def get():
 
 		#-------------
 		# CODE_EDITOR
-		
 
 		"gf_code_editor": {
 			"build_dir_str": f"{modd_str}/../web/build/gf_apps/gf_images",
@@ -82,9 +81,10 @@ def get():
 	
 		"gf_maps": {
 			"build_dir_str": f"{modd_str}/../web/build/gf_apps/gf_images",
-			"files_to_copy_lst": [
-				(f"{modd_str}/../web/src/gf_apps/gf_maps/templates/maps.html", f"{modd_str}/../web/build/gf_apps/gf_images"),
-			]
+			"main_html_path_str": f"{modd_str}/../web/src/gf_apps/gf_maps/templates/gf_maps.html",
+			# "files_to_copy_lst": [
+			# 	(f"{modd_str}/../web/src/gf_apps/gf_maps/templates/maps.html", f"{modd_str}/../web/build/gf_apps/gf_images"),
+			# ]
 		}
 
 		#-------------
